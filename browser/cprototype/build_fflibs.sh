e76f6b91420bf494047fc3a0ca7cf76b86e0ca15
Marvin Poul <ponder@creshal.de>
2015-01-16 22:06:51 +0100

    fiddle with the build script

--- browser/cprototype/build_fflibs.sh
+++ browser/cprototype/build_fflibs.sh
@@ -3,7 +3,7 @@ cd ffmpeg
 emconfigure ./configure --cc="emcc" --enable-cross-compile --target-os=none --arch=x86_32 --cpu=generic \
   --disable-programs --disable-inline-asm --disable-asm --disable-doc --disable-pthreads --disable-w32threads --disable-network \
   --disable-hwaccels --disable-parsers --disable-bsfs --disable-debug --disable-protocols --enable-protocol=file \
-  --disable-indevs --disable-static --enable-shared --extra-cflags="-Wno-warn-absolute-paths"
+  --disable-indevs --extra-cflags="-Wno-warn-absolute-paths"
 emmake make
 
 for l in libavformat libavdevice libavcodec libavutil; do
